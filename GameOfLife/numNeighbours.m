03592b39663cd93bfbf0fa7cbca4849ef01c39ba
vn150 <vn150@BEST042.nb-labs.rutgers.edu>
2017-11-29 17:01:23 -0500

    mild documentation

--- GameOfLife/numNeighbours.m
+++ GameOfLife/numNeighbours.m
@@ -1,9 +1,9 @@
 function [live] = numNeighbours(grid, row, col)
-%NUMNEIGHBOURS Summary of this function goes here
-%   Detailed explanation goes here
+%NUMNEIGHBOURS This program returns the number of living neighbours around
+%a given cell
     live = 0;
     for i = row - 1:row + 1
-        for j = col - 1:col + 1
+        for j = col - 1:col + 1         %In the case that the cell is an edge cell or a corner cell, the try-catch statement makes sure an error isn't thrown by trying to access an index that doesn't exist.
             try
                 if grid(i, j)
                     live = live + 1;
--- GameOfLife/run.m
+++ GameOfLife/run.m
@@ -1,30 +1,5 @@
 function varargout = run(varargin)
-% RUN MATLAB code for run.fig
-%      RUN, by itself, creates a new RUN or raises the existing
-%      singleton*.
-%
-%      H = RUN returns the handle to a new RUN or the handle to
-%      the existing singleton*.
-%
-%      RUN('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in RUN.M with the given input arguments.
-%
-%      RUN('Property','Value',...) creates a new RUN or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before run_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to run_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help run
-
-% Last Modified by GUIDE v2.5 29-Nov-2017 16:35:37
-
-% Begin initialization code - DO NOT EDIT
+% begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -46,51 +21,22 @@ end
 
 % --- Executes just before run is made visible.
 function run_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to run (see VARARGIN)
-
-% Choose default command line output for run
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes run wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
-
 % --- Outputs from this function are returned to the command line.
 function varargout = run_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
 
 
 function location_Callback(hObject, eventdata, handles)
-% hObject    handle to location (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of location as text
-%        str2double(get(hObject,'String')) returns contents of location as a double
 
 
 % --- Executes during object creation, after setting all properties.
 function location_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to location (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -98,9 +44,6 @@ end
 
 % --- Executes on button press in readFile.
 function readFile_Callback(hObject, eventdata, handles)
-% hObject    handle to readFile (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 load(handles.location.String);
 figure,
 ImageVisualization(A, (1-get(handles.speed, 'Value')) * 2);
@@ -108,21 +51,10 @@ ImageVisualization(A, (1-get(handles.speed, 'Value')) * 2);
 
 % --- Executes on slider movement.
 function speed_Callback(hObject, eventdata, handles)
-% hObject    handle to speed (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
 
 
 % --- Executes during object creation, after setting all properties.
 function speed_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to speed (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: slider controls usually have a light gray background.
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
