d06786b8e36acff70bfa25b9191da17ab2716882
Sai <saikiran.nakka@gmail.com>
2018-02-26 16:15:40 -0500

    matlab 2

---
+++ Linear Algebra/rmat.m
@@ -0,0 +1,6 @@
+function A = rmat(m,n)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+A = fix(10*rand(m,n));
+end
+
---
+++ Linear Algebra/rvect.m
@@ -0,0 +1,6 @@
+function [v] = rvect(m)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+v=fix(10*rand(m,1));
+end
+
